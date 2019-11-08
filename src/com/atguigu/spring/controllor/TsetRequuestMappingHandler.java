c16c3c8109a483a37978a89ffbb5b82a54dbd149
wenxuan <192.@163>
2019-11-08 20:35:48 +0800

    up

---
+++ src/com/atguigu/spring/controllor/TsetRequuestMappingHandler.java
@@ -0,0 +1,17 @@
+package com.atguigu.spring.controllor;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+
+
+@RequestMapping(value="/test")
+@Controller
+public class TsetRequuestMappingHandler {
+	@RequestMapping(value= {"/hello","/hello2"},method= {RequestMethod.GET})
+	public String handle1() {
+		System.out.println("处理了请求");
+		return "scuss";
+	}
+}
